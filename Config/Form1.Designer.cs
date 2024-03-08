88636495b360fda7081a033f4fd353ff8b2f1cd8
Jason Leon <jasonleon01@outlook.com>
2024-03-08 22:34:45 +0800

    update

--- Config/Form1.Designer.cs
+++ Config/Form1.Designer.cs
@@ -41,6 +41,8 @@
             button3 = new Button();
             label2 = new Label();
             comboBox2 = new ComboBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
@@ -49,7 +51,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(9, 92);
+            label1.Location = new Point(9, 65);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
             label1.Size = new Size(44, 17);
@@ -61,7 +63,7 @@
             comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "640 x 480", "800 x 600", "960 x 720", "1120 x 840", "1280 x 960" });
-            comboBox1.Location = new Point(80, 89);
+            comboBox1.Location = new Point(80, 62);
             comboBox1.Margin = new Padding(2, 3, 2, 3);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(247, 25);
@@ -79,11 +81,12 @@
             // 
             // pictureBox1
             // 
-            pictureBox1.Image = Properties.Resources.config;
+            pictureBox1.Image = Properties.Resources.back;
             pictureBox1.Location = new Point(6, 8);
             pictureBox1.Margin = new Padding(2, 3, 2, 3);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(345, 69);
+            pictureBox1.Size = new Size(345, 49);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 3;
             pictureBox1.TabStop = false;
             // 
@@ -149,29 +152,51 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(9, 132);
+            label2.Location = new Point(9, 96);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(56, 17);
+            label2.Size = new Size(32, 17);
             label2.TabIndex = 10;
-            label2.Text = "刷新频率";
+            label2.Text = "帧率";
             // 
             // comboBox2
             // 
             comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox2.FormattingEnabled = true;
-            comboBox2.Items.AddRange(new object[] { "16ms/次", "24ms/次", "32ms/次", "40ms/次", "48ms/次" });
-            comboBox2.Location = new Point(80, 129);
+            comboBox2.Items.AddRange(new object[] { "15", "30", "45", "60" });
+            comboBox2.Location = new Point(80, 93);
             comboBox2.Margin = new Padding(2, 3, 2, 3);
             comboBox2.Name = "comboBox2";
             comboBox2.Size = new Size(247, 25);
             comboBox2.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(80, 133);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(75, 21);
+            checkBox1.TabIndex = 12;
+            checkBox1.Text = "垂直同步";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(161, 133);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(51, 21);
+            checkBox2.TabIndex = 13;
+            checkBox2.Text = "柔化";
+            checkBox2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
             ClientSize = new Size(352, 320);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
             Controls.Add(comboBox2);
             Controls.Add(label2);
             Controls.Add(button3);
@@ -214,5 +239,7 @@
         private Button button3;
         private Label label2;
         private ComboBox comboBox2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
     }
 }
--- Config/Form1.cs
+++ Config/Form1.cs
@@ -18,18 +18,22 @@ namespace Config
             data = datatext.Split(Environment.NewLine.ToCharArray());
             data = data.Where(s => !string.IsNullOrEmpty(s)).ToArray();
             comboBox1.SelectedIndex = (int)((float.Parse(data[1].Split("=")[1]) - 1) / 0.25);
-            comboBox2.SelectedIndex = (int.Parse(data[2].Split("=")[1]) - 16) / 8;
+            comboBox2.SelectedIndex = int.Parse(data[2].Split("=")[1]);
             trackBar1.Value = int.Parse(data[3].Split("=")[1]) / 10;
             trackBar2.Value = int.Parse(data[4].Split("=")[1]) / 10;
+            checkBox1.Checked = int.Parse(data[5].Split("=")[1]) != 0;
+            checkBox2.Checked = int.Parse(data[6].Split("=")[1]) != 0;
         }
         private void button1_Click(object sender, EventArgs e)
         {
             string savedata = "";
             savedata = savedata + "[config]\n";
             savedata = savedata + "resolutionRatio=" + (comboBox1.SelectedIndex * 0.25 + 1).ToString() + "\n";
-            savedata = savedata + "frameRate=" + (comboBox2.SelectedIndex * 8 + 16).ToString() + "\n";
+            savedata = savedata + "frameRate=" + comboBox2.SelectedIndex.ToString() + "\n";
             savedata = savedata + "BGMVolume=" + (trackBar1.Value * 10).ToString() + "\n";
             savedata = savedata + "SEVolume=" + (trackBar2.Value * 10).ToString() + "\n";
+            savedata = savedata + "verticalSync=" + (checkBox1.Checked ? "1" : "0") + "\n";
+            savedata = savedata + "smooth=" + (checkBox2.Checked ? "1" : "0") + "\n";
             System.IO.File.WriteAllText(Application.StartupPath + @"ref\config.ini", savedata, Encoding.GetEncoding("GB2312"));
         }
 
@@ -43,6 +47,5 @@ namespace Config
         {
             Environment.Exit(0);
         }
-
     }
 }
